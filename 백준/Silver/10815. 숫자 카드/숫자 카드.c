f428e43c058b23319fd4204807c7ef4f5ce2ce67
Badak1227 <105299971+Badak1227@users.noreply.github.com>
2023-04-01 23:22:49 +0900

    [Silver V] Title: 숫자 카드, Time: 348 ms, Memory: 79236 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.c
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.c
@@ -1,75 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-typedef struct tree {
-    struct tree* prev;
-    long long int data;
-    struct tree* next;
-}tree;
-
-int contain(long long int num1, tree* groupA) {
-    if (groupA->data== num1) {
-        return 1;
-    }
-    if (groupA->data < num1) {
-        if (groupA->next != NULL) {
-            return contain(num1, groupA->next);
-        }
-    }
-    else {
-        if (groupA->prev != NULL) {
-            return contain(num1, groupA->prev);
-        }
-    }
-    
-    return 0;
-}
-
-void add(tree* cur, tree* new) {
-    if ((cur->data) < (new->data)) {
-        if (cur->next != NULL) {
-            add(cur->next, new);
-        }
-        else {
-            cur->next = new;
-        }
-    }
-    else {
-        if (cur->prev != NULL) {
-            add(cur->prev, new);
-        }
-        else {
-            cur->prev = new;
-        }
-    }
-}
+int nums[20000001] = { 0 };
 
 int main() {
     int N;
     scanf("%d", &N);
-    
-    tree* head = (tree*)malloc(sizeof(tree));
-    long long int num1;
-    scanf("%lld", &num1);
-    head->data = num1;
-    head->next = NULL;
-    head->prev = NULL;
 
-    for (int i = 1; i < N; i++) {
-        tree* tmp = (tree*)malloc(sizeof(tree));
-        scanf("%lld", &num1);
-        tmp->data = num1;
-        tmp->next = NULL;
-        tmp->prev = NULL;
-        add(head, tmp);
+    for (int i = 0; i < N; i++) {
+        int num;
+        scanf("%d", &num);
+        nums[num + 10000000] = 1;
     }
-
+    
     int M;
     scanf("%d", &M);
     for (int i = 0; i < M; i++) {
-        long long int num2;
-        scanf("%lld", &num2);
-        int result = contain(num2, head);
-        printf("%d ", result);
+        int num;
+        scanf("%d", &num);
+        printf("%d ", nums[num + 10000000]);
     }
 }
