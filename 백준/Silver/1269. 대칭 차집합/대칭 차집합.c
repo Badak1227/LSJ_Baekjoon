effbab61c86ea56c25fdc1a0f0bdf9cb7e1d468e
Badak1227 <105299971+Badak1227@users.noreply.github.com>
2023-04-07 23:08:22 +0900

    [Silver IV] Title: 대칭 차집합, Time: 104 ms, Memory: 2560 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.c
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.c
@@ -1,35 +1,61 @@
 #include <stdio.h>
 
-char bit_map[12500001] = { 0 };
+void quick(int group[], int start, int end) {
+    int l = start, r = end, m = group[(l + end) / 2];
+    while (l <= r) {
+        while (group[l] < m) {
+            l++;
+        }
+        while (group[r] > m) {
+            r--;
+        }
 
-int count = 0;
+        if (l <= r) {
+            int tmp = group[l];
+            group[l] = group[r];
+            group[r] = tmp;
 
-void mapping(int num) {
-	int bit_map_index = num / 8;
-	int bit_index = num % 8;
-	if (bit_map[bit_map_index] & 1 << bit_index) {
-		bit_map[bit_map_index] &= ~(1 << bit_index);
-		count--;
-	}
-	else {
-		bit_map[bit_map_index] |= 1 << bit_index;
-		count++;
-	}
+            l++;
+            r--;
+        }
+    }
+    if (start < r) {
+        quick(group, start, r);
+    }
+    if (l < end) {
+        quick(group, l, end);
+    }
 }
 
 int main() {
-	int N, M;
-	
-	scanf("%d %d", &N, &M);
-	for (int i = 0; i < N; i++) {
-		int tmp;
-		scanf("%d", &tmp);
-		mapping(tmp);
-	}
-	for (int i = 0; i < M; i++) {
-		int tmp;
-		scanf("%d", &tmp);
-		mapping(tmp);
-	}
-	printf("%d", count);
+    int N, M;
+    int A[200001] = { 0 }, B[200001] = { 0 };
+
+    scanf("%d %d", &N, &M);
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &A[i]);
+    }
+    for (int i = 0; i < M; i++) {
+        scanf("%d", &B[i]);
+    }
+
+    quick(A, 0, N - 1);
+    quick(B, 0, M - 1);
+
+    int Aindex = 0, Bindex = 0, count = 0;
+    while (A[Aindex] && B[Bindex]) {
+        if (A[Aindex] == B[Bindex]) {
+            count++;
+            Aindex++;
+            Bindex++;
+        }
+        else if (A[Aindex] > B[Bindex]) {
+            Bindex++;
+        }
+        else {
+            Aindex++;
+        }
+    }
+
+    printf("%d", (N + M) - count * 2);
 }
