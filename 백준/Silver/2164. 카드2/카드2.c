eeadbd9059172a17d7f6cb54f4313d4ddc98fae1
Badak1227 <105299971+Badak1227@users.noreply.github.com>
2023-04-17 21:25:04 +0900

    [Silver IV] Title: 카드2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.c
+++ 백준/Silver/2164. 카드2/카드2.c
@@ -1,46 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-typedef struct list {
-	int num;
-	struct list* next;
-}list;
-
-list* new_list(int num) {
-	list* tmp = (list*)malloc(sizeof(list));
-	tmp->num = num;
-	tmp->next = NULL;
-	return tmp;
-}
 
 int main() {
 	int N;
 	scanf("%d", &N);
 
-	list* front = new_list(1);
-	list* rear = front;
-	for (int i = 2; i <= N; i++) {
-		front->next = new_list(i);
-		front = front->next;
+	int i = 1;
+	while (i < N) {
+		i <<= 1;
 	}
-	
-	while (front != rear) {
-		list* tmp = rear;
-		rear = rear->next;
-		free(tmp);
-
-		front->next = rear;
-
-		rear = rear->next;
-
-		front = front->next;
-
-		front->next = NULL;
-
-		list* tmp1 = rear;
-	}
-
-	printf("%d", rear->num);
-
-	free(rear);
+	printf("%d", N + N - i);
 }
