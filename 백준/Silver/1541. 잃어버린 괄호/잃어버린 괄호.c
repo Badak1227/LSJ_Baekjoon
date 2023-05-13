03b4c491586c994bd0c24e0498bf6e9e6a27b820
Badak1227 <105299971+Badak1227@users.noreply.github.com>
2023-05-13 22:09:41 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.c
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.c
@@ -1,77 +1,37 @@
 #include <stdio.h>
 
-char calc[51] = { 0 };
+int main() {
+	
+	int on = 1;
+	int sum = 0;
 
-int stack[51] = { 0 };
-int send = 0;
+	scanf("%d", &sum);
 
-int operator[51] = { 0 };
-int oend = 0;
+	while (1) {
+		char operator;
+		int num;
 
-void to_num(int start, int digit) {
-	int res = 0;
+		scanf("%c", &operator);
 
-	for (int i = 1; digit > 0; digit--, i *= 10) {
-		res += (calc[start--] - '0') * i;
-	}
-
-	stack[send++] = res;
-}
-
-void calculate() {
-	while (oend != 0) {
-		if (operator[oend - 1] == '+') {
-			stack[send - 2] = stack[send - 2] + stack[send - 1];
-
-			send--;
-			oend--;
-		}
-		else {
-			stack[send - 2] = stack[send - 2] - stack[send - 1];
-
-			send--;
-			oend--;
-		}
-	}
-}
-
-int Calc() {
-
-	int digit = 0;
-	int i = 0;
-
-	for (i = 0; calc[i] != 0; i++) {
+		if (operator == '\n') break;
 		
-		if (calc[i] >= '0' && calc[i] <= '9') {
-			digit++;
-		}
-		else {
-			to_num(i - 1, digit);
-			digit = 0;
+		scanf("%d", &num);
 
-			if (calc[i] == '-') {
-				calculate();
-				operator[oend++] = '-';
+		if (on) {
+			if (operator == '+') {
+				sum += num;
 			}
 			else {
-				operator[oend++] = '+';
+				sum -= num;
+				on--;
 			}
+		}
+		else {
+			sum -= num;
 		}
 	}
 
-	to_num(i - 1, digit);
-	calculate();
-}
-
-int main() {
-	scanf("%s", calc);
-
-	Calc();
-
-	for (int i = 0; i < send; i++) {
-		printf("%d ", stack[i]);
-	}
-
+	printf("%d", sum);
 
 	return 0;
 }
