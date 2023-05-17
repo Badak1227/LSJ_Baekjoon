bc247ce8eef357db98baea4412f536da70e034fc
Badak1227 <105299971+Badak1227@users.noreply.github.com>
2023-05-17 23:03:06 +0900

    [Silver III] Title: 01타일, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.c
+++ 백준/Silver/1904. 01타일/01타일.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-	int N, p1=1, p2=2;
+	int N, p1=1, p2=2, tmp;
 
 	scanf("%d" ,&N);
 
@@ -11,9 +11,11 @@ int main() {
 	}
 
 	for (int i = 3; i <= N; i++) {
-		int tmp = p2;
-		p2 = (p2 + p1) % 15746;
+		tmp = p2;
+		p2 += p1;
 		p1 = tmp;
+        
+        if(p2 >= 15746) p2 -= 15746;
 	}
 
 	printf("%d", p2);
