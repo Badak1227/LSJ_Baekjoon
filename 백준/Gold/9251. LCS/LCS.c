b911034606e59f7426753bc20f228db95b1913de
Badak1227 <105299971+Badak1227@users.noreply.github.com>
2023-05-28 22:53:10 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.c
+++ 백준/Gold/9251. LCS/LCS.c
@@ -3,17 +3,17 @@
 
 int main() {
 	char A[1001] = { 0 }, B[1001] = { 0 };
-	int dp[1001][1001] = { 0 };
+	int dp[2][1001] = { 0 };
 	scanf("%s", A);
 	scanf("%s", B);
 
 	int i = 0, j = 0;
 	for (i; B[i] != 0; i++) {
 		for (j = 0; A[j] != 0; j++) {
-			if (B[i] == A[j]) dp[i + 1][j + 1] = dp[i][j] + 1;
-			else dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1]);
+			if (B[i] == A[j]) dp[(i + 1) % 2][j + 1] = dp[i % 2][j] + 1;
+			else dp[(i + 1) % 2][j + 1] = max(dp[(i + 1) % 2][j], dp[i % 2][j + 1]);
 		}
 	}
 
-	printf("%d", dp[i][j]);
+	printf("%d", dp[i % 2][j]);
 }
