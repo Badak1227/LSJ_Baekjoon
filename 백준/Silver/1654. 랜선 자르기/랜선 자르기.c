824eb50ff2d5d501ea36eefc028e091cb4c13564
Badak1227 <105299971+Badak1227@users.noreply.github.com>
2023-06-14 19:43:54 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void bsearch(long long LAN[], long long max, int N) {
+	long long l = 1, r = max, count = 0;
+
+	while (l <= r) {
+		count = 0;
+
+		long long m = l + (r - l) / 2;
+
+		int i = 0;
+		while (LAN[i] != 0) {
+			count += LAN[i++] / m;
+		}
+
+		if (count < N) {
+			r = m - 1;
+		}
+		else {
+			l = m + 1;
+		}
+	}
+
+	printf("%lld", r);
+}
+
+int main() {
+	int K, N;
+	long long LAN[10001] = { 0 }, max = 0;
+
+	scanf("%d %d", &K, &N);
+
+	for (int i = 0; i < K; i++) {
+		scanf("%lld", &LAN[i]);
+
+		max = LAN[i] > max ? LAN[i] : max;
+	}
+
+	bsearch(LAN, max, N);
+}
