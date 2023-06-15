f0dee32ecc9169cfaeb4a89bcdddf91707e77108
Badak1227 <105299971+Badak1227@users.noreply.github.com>
2023-06-15 16:01:02 +0900

    [Silver II] Title: 나무 자르기, Time: 240 ms, Memory: 4900 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+	int N, M, max = 0,trees[1000000] = { 0 };
+
+	scanf("%d %d", &N, &M);
+
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &trees[i]);
+
+		max = trees[i] > max ? trees[i] : max;
+	}
+
+	int l = 0, r = max;
+
+	while (l <= r) {
+		long long len = 0;
+		int m = l + (r - l) / 2;
+
+		for (int i = 0; i < N; i++) {
+			if (trees[i] <= m) continue;
+
+			len += (long long)(trees[i] - m);
+		}
+
+		if (len < M) {
+			r = m - 1;
+		}
+		else {
+			l = m + 1;
+		}
+	}
+
+	printf("%d", r);
+}
