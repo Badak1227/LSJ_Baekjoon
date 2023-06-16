ef47034745b559ca2fcd13a68a7608eff9908536
Badak1227 <105299971+Badak1227@users.noreply.github.com>
2023-06-16 22:18:26 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+	int N;
+	long long stair[2][10] = { {0, 1, 1, 1, 1, 1, 1, 1, 1, 1} ,{0, 0, 0, 0, 0, 0, 0, 0, 0, 0} };
+
+	scanf("%d", &N);
+
+	for (int i = 0; i < N - 1; i++) {
+		int cur = i % 2, next = (i + 1) % 2;
+
+		stair[next][0] = stair[cur][1] % 1000000000;
+		stair[next][9] = stair[cur][8] % 1000000000;
+
+		for (int j = 1; j < 9; j++) {
+			stair[next][j] = (stair[cur][j - 1] + stair[cur][j + 1]) % 1000000000;
+		}
+	}
+
+	int cur = (N - 1) % 2;
+	long long answer = 0;
+
+	for (int i = 0; i < 10; i++) {
+		answer += stair[cur][i] % 1000000000;
+	}
+
+	printf("%lld", answer % 1000000000);
+}
