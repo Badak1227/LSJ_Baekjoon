ac557c77ae96ba0f7736162e16e87fbafec4bfa4
Badak1227 <105299971+Badak1227@users.noreply.github.com>
2023-06-26 23:49:32 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 1192 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.c
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.c
@@ -1,40 +1,31 @@
 #include <stdio.h>
 
-int sol2(int cabbage[][52], int row, int column) {
+int sol2(int cabbage[][51], int row, int column) {
+
+	if (cabbage[row][column] == 0 || row < 0 || column < 0) return;
 
 	cabbage[row][column] = 0;
 
-	if (cabbage[row + 1][column] == 1) {
-		sol2(cabbage, row + 1, column);
-	}
+	sol2(cabbage, row + 1, column);
 	
-	if (cabbage[row][column + 1] == 1) {
-		sol2(cabbage, row, column + 1);
-	}
+	sol2(cabbage, row, column + 1);
 
-	if (cabbage[row - 1][column] == 1) {
-		sol2(cabbage, row - 1, column);
-	}
+	sol2(cabbage, row - 1, column);
 
-	if (cabbage[row][column - 1] == 1) {
-		sol2(cabbage, row, column - 1);
-	}
+	sol2(cabbage, row, column - 1);
 }
 
 void sol(int M, int N, int K) {
-	int row, column, cabbage[52][52] = {0}, cnt = 0;
+	int row, column, cabbage[51][51] = {0}, cnt = 0;
 
 	while (K--) {
 		scanf("%d %d", &column, &row);
-
-		column++;
-		row++;
-
+        
 		cabbage[row][column] = 1;
 	}
 
-	for (int i = 1; i < N + 1; i++) {
-		for (int j = 1; j < M + 1; j++) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
 			
 			if (cabbage[i][j] == 1) {
 				sol2(cabbage, i, j);
