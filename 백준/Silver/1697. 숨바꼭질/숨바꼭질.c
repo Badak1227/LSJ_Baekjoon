d4531a3bf2502c3f6a9bb947c58116af2ec52862
Badak1227 <105299971+Badak1227@users.noreply.github.com>
2023-07-03 00:03:15 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.c
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.c
@@ -1,61 +1,21 @@
 #include <stdio.h>
 
-int visited[100001] = { 0 }, queue[100002] = { 0 }, start = 0, end = 0;
-
-void enqueue(int N) {
-	queue[end] = N;
-	end = (end + 1) % 100002;
-
-	visited[N] = 1;
+int min(int a, int b) {
+	return a < b ? a : b;
 }
 
-int dequeue() {
-	int res = queue[start];
-	start = (start + 1) % 100002;
-
-	return res;
-}
-
-void sol(int N, int K) {
-	int cnt = 0, len = 1;
-
-	enqueue(N);
-
-	while (len) {
-		int tmplen = 0;
-
-		while (len--) {
-			int num = dequeue();
-
-			if (num == K) {
-				printf("%d", cnt);
-				return;
-			}
-
-			if (num - 1 >= 0) {
-				if (visited[num - 1] != 1) {
-					enqueue(num - 1);
-					tmplen++;
-				}
-			}
-
-			if (num + 1 <= 100000) {
-				if (visited[num + 1] != 1) {
-					enqueue(num + 1);
-					tmplen++;
-				}
-			}
-
-			if (num * 2 <= 100000) {
-				if (visited[num * 2] != 1) {
-					enqueue(num * 2);
-					tmplen++;
-				}
-			}
-		}
-
-		len = tmplen;
-		cnt++;
+int sol(int N, int K) {
+	if (K <= N) {
+		return N - K;
+	}
+	else if (K == 1) {
+		return 1;
+	}
+	else if (K % 2 == 1) {
+		return 1 + min(sol(N, K - 1), sol(N, K + 1));
+	}
+	else {
+		return min(K - N, 1 + sol(N, K / 2));
 	}
 }
 
@@ -64,5 +24,5 @@ int main() {
 
 	scanf("%d %d", &N, &K);
 
-	sol(N, K);
+	printf("%d", sol(N, K));
 }
