8c673523d299cc0a5ea744deb3031e961c8d2e47
Badak1227 <105299971+Badak1227@users.noreply.github.com>
2023-07-10 13:42:41 +0900

    [Silver II] Title: A → B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.c
+++ 백준/Silver/16953. A → B/A → B.c
@@ -1,126 +1,34 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-typedef struct node {
-    int data;
-    struct node* next;
-}node;
+int cnt = 0;
 
-typedef struct queue {
-    node* start;
-    node* end;
-}queue;
+void sol(int A, int B) {
+    cnt++;
 
-char visited[125000001] = { 0 };
-
-queue num;
-
-void input(int next) {
-    visited[next / 8] |= 1 << (next % 8);
-
-    if (num.start == NULL && num.end == NULL) {
-        num.start = (node*)malloc(sizeof(node));
-
-        num.start->data = next;
-        num.start->next = NULL;
-
-        num.end = num.start;
+    if (A == B) {
+        printf("%d", cnt);
         return;
     }
 
-    num.end->next = (node*)malloc(sizeof(node));
-    num.end = num.end->next;
-
-    num.end->data = next;
-    num.end->next = NULL;
-}
-
-void delete() {
-    node* tmp = num.start;
-
-    num.start = num.start->next;
-
-    if (num.start == NULL) num.end = num.start;
-
-    free(tmp);
-}
-
-void freequeue() {
-
-    while (num.start != NULL) {
-        node* tmp = num.start;
-
-        num.start = num.start->next;
-
-        free(tmp);
+    if (A > B) {
+        printf("-1");
     }
-}
-
-void sol(long long A, long long B) {
-    
-    num.start = NULL;
-    num.end = NULL;
-
-    int len = 1, ans = 1;
-    
-    if(A == B){
-        printf("%d", ans);
-        return;
+    else if (B % 2 == 0) {
+        sol(A, B / 2);
     }
-    
-    input(A);
-
-    while (len) {
-        int tmplen = 0;
-
-        while (len--) {
-            long long cur = num.start->data;
-
-            delete();
-
-            if (cur * 10 + 1 <= B) {
-
-                if (cur * 10 + 1 == B) {
-                    printf("%d", ans + 1);
-                    freequeue();
-                    return;
-                }
-
-                if (!(visited[(cur * 10 + 1) / 8] & 1 << (cur * 10 + 1) % 8)) {
-                    input(cur * 10 + 1);
-                    tmplen++;
-                }
-            }
-            
-            if (cur * 2 <= B) {
-
-                if (cur * 2 == B) {
-                    printf("%d", ans + 1);
-                    freequeue();
-                    return;
-                }
-
-                if (!(visited[(cur * 2) / 8] & 1 << (cur * 2) % 8)) {
-                    input(cur * 2);
-                    tmplen++;
-                }
-            }
-            
-            
-        }
-
-        len = tmplen;
-        ans++;
+    else if (B % 10 == 1) {
+        sol(A, B / 10);
     }
-
-    printf("-1");
+    else {
+        printf("-1");
+    }
 }
 
 int main() {
-    long long A, B;
+    int A, B;
     
 
-    scanf("%lld %lld", &A, &B);
+    scanf("%d %d", &A, &B);
 
     sol(A, B);
 }
