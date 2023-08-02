27eb09b41528ef5288d22b4cbde1d9685c0dbda0
Badak1227 <105299971+Badak1227@users.noreply.github.com>
2023-08-02 23:57:44 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    long long A, B, C;
+    scanf("%lld %lld %lld", &A, &B, &C);
+
+    long long sum = A + B + C;
+    printf("%lld\n", sum);
+
+    return 0;
+}
