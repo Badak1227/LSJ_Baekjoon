bc4dccc68d32483089fbbaa00fd2a37a3a0b3a9c
Badak1227 <105299971+Badak1227@users.noreply.github.com>
2024-04-08 19:41:12 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 32 ms, Memory: 18220 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.c
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.c
@@ -12,15 +12,14 @@ char board[601][601] = { 0 }, movement[4][2] = {
 int dfs(int y, int x) {
 	if (board[y][x] == 'P') answer++;
 
-	board[y][x] = 'I';
+	board[y][x] = 'X';
 
 	for (int i = 0; i < 4; i++) {
 		int next_y = y + movement[i][0], next_x = x + movement[i][1];
 
-		if (0 <= next_y && next_y < N && 0 <= next_x && next_x < M && board[next_y][next_x] != 'X' && board[next_y][next_x] != 'I') {
-			
-			dfs(next_y, next_x);
-		}
+		if (0 > next_y || next_y >= N || 0 > next_x || next_x >= M || board[next_y][next_x] == 'X') continue;
+		
+		dfs(next_y, next_x);
 	}
 }
 
